a2249e2ed34446de8cddec694cdb36de70b637f7
halkn <nhalkn0920@gmail.com>
2026-07-18 18:56:01 +0900

    feat: render headings and paragraphs

--- src/markdown.rs
+++ src/markdown.rs
@@ -1,3 +1,5 @@
+use pulldown_cmark::{Event, HeadingLevel as ParserHeadingLevel, Parser, Tag, TagEnd};
+
 #[derive(Clone, Debug, Default, PartialEq, Eq)]
 pub struct Document {
     blocks: Vec<Block>,
@@ -10,6 +12,70 @@ impl Document {
 
     pub fn blocks(&self) -> &[Block] {
         &self.blocks
+    }
+}
+
+pub fn parse(source: &str) -> Document {
+    let mut blocks = Vec::new();
+    let mut active_block = None;
+
+    for event in Parser::new(source) {
+        match event {
+            Event::Start(Tag::Heading { level, .. }) => {
+                active_block = Some(ActiveBlock::Heading {
+                    level: heading_level(level),
+                    content: Vec::new(),
+                });
+            }
+            Event::End(TagEnd::Heading(_)) => {
+                if let Some(ActiveBlock::Heading { level, content }) = active_block.take() {
+                    blocks.push(Block::Heading { level, content });
+                }
+            }
+            Event::Start(Tag::Paragraph) => {
+                active_block = Some(ActiveBlock::Paragraph(Vec::new()));
+            }
+            Event::End(TagEnd::Paragraph) => {
+                if let Some(ActiveBlock::Paragraph(content)) = active_block.take() {
+                    blocks.push(Block::Paragraph(content));
+                }
+            }
+            Event::Text(text) => push_inline(&mut active_block, Inline::Text(text.into_string())),
+            Event::Code(code) => push_inline(&mut active_block, Inline::Code(code.into_string())),
+            Event::SoftBreak => push_inline(&mut active_block, Inline::SoftBreak),
+            Event::HardBreak => push_inline(&mut active_block, Inline::HardBreak),
+            _ => {}
+        }
+    }
+
+    Document::new(blocks)
+}
+
+enum ActiveBlock {
+    Heading {
+        level: HeadingLevel,
+        content: Vec<Inline>,
+    },
+    Paragraph(Vec<Inline>),
+}
+
+fn push_inline(active_block: &mut Option<ActiveBlock>, inline: Inline) {
+    match active_block {
+        Some(ActiveBlock::Heading { content, .. }) | Some(ActiveBlock::Paragraph(content)) => {
+            content.push(inline)
+        }
+        None => {}
+    }
+}
+
+fn heading_level(level: ParserHeadingLevel) -> HeadingLevel {
+    match level {
+        ParserHeadingLevel::H1 => HeadingLevel::One,
+        ParserHeadingLevel::H2 => HeadingLevel::Two,
+        ParserHeadingLevel::H3 => HeadingLevel::Three,
+        ParserHeadingLevel::H4 => HeadingLevel::Four,
+        ParserHeadingLevel::H5 => HeadingLevel::Five,
+        ParserHeadingLevel::H6 => HeadingLevel::Six,
     }
 }
 
@@ -74,7 +140,23 @@ pub enum Inline {
 
 #[cfg(test)]
 mod tests {
-    use super::{Block, Document, HeadingLevel, Inline, ListItem};
+    use super::{Block, Document, HeadingLevel, Inline, ListItem, parse};
+
+    #[test]
+    fn parses_headings_and_paragraphs() {
+        let document = parse("# DocSail\n\nA terminal Markdown viewer.");
+
+        assert_eq!(
+            document.blocks(),
+            [
+                Block::Heading {
+                    level: HeadingLevel::One,
+                    content: vec![Inline::Text("DocSail".to_owned())],
+                },
+                Block::Paragraph(vec![Inline::Text("A terminal Markdown viewer.".to_owned())]),
+            ]
+        );
+    }
 
     #[test]
     fn preserves_blocks_and_nested_gfm_content_without_rendering_types() {
